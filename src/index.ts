043dc54e63db1b2e51f312f4039f415e1f049776
Zhongliang Wang <zwang@rational.studio>
2023-07-02 23:53:27 +0800

    update re-exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-export { type PositionTransformer } from './positionTransform';
+export {
+  type PositionTransformer,
+  followDraggableItem,
+} from './positionTransform';
+export * from './collisionDetection';
 export { useDraggable } from './hooks/useDraggable';
 export { useDroppable } from './hooks/useDroppable';
+export { default as DNDOverlay } from './components/DNDOverlay';
+export { default as DNDScope } from './components/DNDScope';
 export type {
   DataPayload,
   DraggableItem,
