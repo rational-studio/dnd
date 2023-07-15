05a5aafc04d60cdb4fa2e889fed86271d5104fb6
Zhongliang Wang <zwang@rational.studio>
2023-07-15 11:53:43 +0800

    restructure to support tauri

---
+++ src/hooks/useBaseDraggable.ts
@@ -0,0 +1,154 @@
+import { useCallback, useEffect, useId, useRef, useState } from 'react';
+
+import { distanceBetween } from '../collisionDetection/algorithms';
+import { useInternalDNDScope } from '../components/DNDScope';
+import {
+  type Coord,
+  type DataPayload,
+  type DraggableItem,
+  type Rect,
+} from '../store';
+import type { CommonDraggableOptions } from './typings';
+import { getDroppableCollisionUpdate } from './utils';
+
+interface BaseDraggableOptions<DraggablePayload extends DataPayload>
+  extends CommonDraggableOptions<DraggablePayload> {
+  resizeElementBoundingClientRect: () => Rect | undefined;
+  boundingClientRect: () => Rect;
+}
+
+export function useBaseDraggable<DraggablePayload extends DataPayload>(
+  options: BaseDraggableOptions<DraggablePayload>,
+  cleanupFn: () => void
+) {
+  const _identity = useId();
+  const {
+    payload: { type, data },
+    resizeElementBoundingClientRect,
+    boundingClientRect,
+    animated = false,
+    cursor = 'default',
+    action = 'move',
+    axis = 'xy',
+  } = options;
+
+  const { store, distanceConstraint, collisionDetectionRef, onDragStartRef } =
+    useInternalDNDScope();
+
+  const [isActiveDraggable, setIsActiveDraggable] = useState(
+    () => store.getState().activeDraggable?._identity === _identity
+  );
+
+  // TODO: Strip for production-build
+  if ((action === 'resize-start' || action === 'resize-end') && axis === 'xy') {
+    throw new Error(
+      `Axis 'xy' is not supported for resize actions. Use 'x' or 'y' instead.`
+    );
+  }
+
+  useEffect(
+    () =>
+      store.subscribe(
+        state => state.activeDraggable,
+        activeDraggable => {
+          if (activeDraggable && activeDraggable._identity === _identity) {
+            setIsActiveDraggable(true);
+          } else {
+            setIsActiveDraggable(false);
+          }
+        }
+      ),
+    [_identity, store]
+  );
+
+  const initialMouseCoord = useRef<Coord>({ x: 0, y: 0 });
+
+  const setInitialMouseCoord = useCallback((mouseCoord: Coord) => {
+    initialMouseCoord.current = mouseCoord;
+  }, []);
+
+  const startDragging = useCallback(
+    (mouseCoord: Coord) => {
+      const distance = distanceBetween(initialMouseCoord.current, mouseCoord);
+
+      if (distance < distanceConstraint) {
+        return;
+      }
+
+      const resizeElementRect = resizeElementBoundingClientRect();
+      const draggableRect =
+        (action === 'resize-end' || action === 'resize-start') &&
+        resizeElementRect
+          ? resizeElementRect
+          : boundingClientRect();
+
+      if (action === 'create') {
+        draggableRect.x =
+          axis === 'x' || axis === 'xy' ? mouseCoord.x : draggableRect.x;
+        draggableRect.y =
+          axis === 'y' || axis === 'xy' ? mouseCoord.y : draggableRect.y;
+      }
+
+      const draggable = {
+        _identity,
+        payload: { type, data },
+        action,
+        animated,
+        cursor,
+        axis,
+        cleanupFn,
+      } satisfies DraggableItem;
+
+      store.setState(state => {
+        // Drag is starting, take a snapshot of Rect of all Droppables
+        const droppableRects = state.droppables.map(item =>
+          item.boundingClientRect()
+        );
+        const droppableUpdater = getDroppableCollisionUpdate(
+          type,
+          state.droppables,
+          draggableRect,
+          mouseCoord,
+          collisionDetectionRef.current
+        );
+        return {
+          activeDraggable: draggable,
+          lastKnownActiveDraggabble: draggable,
+          initialDraggableRect: draggableRect,
+          currentDraggableRect: draggableRect,
+          currentTransformedGhostRect: draggableRect,
+          initialMouseCoord: mouseCoord,
+          currentMouseCoord: mouseCoord,
+          droppableRects,
+          ...droppableUpdater,
+        };
+      });
+
+      // FIXME: This is not onDragStart, it will be called
+      // whenever the pointermove is happening
+      onDragStartRef.current?.(draggable, draggableRect);
+    },
+    [
+      _identity,
+      action,
+      animated,
+      axis,
+      boundingClientRect,
+      cleanupFn,
+      collisionDetectionRef,
+      cursor,
+      data,
+      distanceConstraint,
+      onDragStartRef,
+      resizeElementBoundingClientRect,
+      store,
+      type,
+    ]
+  );
+
+  return {
+    setInitialMouseCoord,
+    startDragging,
+    isActiveDraggable,
+  };
+}
--- src/hooks/useDraggable.ts
+++ src/hooks/useDraggable.ts
@@ -1,74 +1,51 @@
-import {
-  useEffect,
-  useId,
-  useRef,
-  useState,
-  type CSSProperties,
-  type MutableRefObject,
-} from 'react';
+import { useCallback, useEffect, useState, type MutableRefObject } from 'react';
 
-import { distanceBetween } from '../collisionDetection/algorithms';
-import { useInternalDNDScope } from '../components/DNDScope';
-import { type Coord, type DataPayload, type DraggableItem } from '../store';
-import { clientDOMRectToRect, getDroppableCollisionUpdate } from './utils';
+import { type DataPayload } from '../store';
+import type { CommonDraggableOptions } from './typings';
+import { useBaseDraggable } from './useBaseDraggable';
+import { clientDOMRectToRect } from './utils';
 
-type DraggableOptions<DraggablePayload extends DataPayload> = {
-  payload: DraggablePayload;
+interface DraggableOptions<DraggablePayload extends DataPayload>
+  extends CommonDraggableOptions<DraggablePayload> {
   resizeElementRef?: MutableRefObject<HTMLElement | null>;
-  cursor?: CSSProperties['cursor'];
-  action?: 'move' | 'resize-start' | 'resize-end' | 'create';
-  animated?: boolean;
-  axis?: 'x' | 'y' | 'xy';
-  onMove?: (delta: Coord, event: PointerEvent) => void;
-  positionOverlay?: DraggablePayload['type'];
-};
+}
 
-export function useDraggable<DraggablePayload extends DataPayload>(
-  options: DraggableOptions<DraggablePayload>
-) {
-  const _identity = useId();
-  const {
-    payload: { type, data },
-    resizeElementRef,
-    animated = false,
-    cursor = 'default',
-    action = 'move',
-    axis = 'xy',
-    positionOverlay,
-  } = options;
-  const { store, distanceConstraint, collisionDetectionRef, onDragStartRef } =
-    useInternalDNDScope();
-  const [isActiveDraggable, setIsActiveDraggable] = useState(
-    () => store.getState().activeDraggable?._identity === _identity
-  );
+export function useDraggable<DraggablePayload extends DataPayload>({
+  resizeElementRef,
+  ...options
+}: DraggableOptions<DraggablePayload>) {
+  const [preActivationPhase, setPreActivationPhase] = useState(false);
+  const cleanupFn = useCallback(() => setPreActivationPhase(false), []);
   const [draggableElement, setDraggableRef] = useState<HTMLElement | null>(
     null
   );
 
-  // TODO: Strip for production-build
-  if ((action === 'resize-start' || action === 'resize-end') && axis === 'xy') {
-    throw new Error(
-      `Axis 'xy' is not supported for resize actions. Use 'x' or 'y' instead.`
+  const resizeElementBoundingClientRect = useCallback(() => {
+    if (!resizeElementRef || !resizeElementRef.current) {
+      return;
+    }
+    return clientDOMRectToRect(
+      resizeElementRef.current.getBoundingClientRect()
     );
-  }
+  }, [resizeElementRef]);
 
-  useEffect(
-    () =>
-      store.subscribe(
-        state => state.activeDraggable,
-        activeDraggable => {
-          if (activeDraggable && activeDraggable._identity === _identity) {
-            setIsActiveDraggable(true);
-          } else {
-            setIsActiveDraggable(false);
-          }
-        }
-      ),
-    [_identity, store]
-  );
+  const boundingClientRect = useCallback(() => {
+    if (!draggableElement) {
+      return {
+        x: 0,
+        y: 0,
+        w: 0,
+        h: 0,
+      };
+    }
+    return clientDOMRectToRect(draggableElement.getBoundingClientRect());
+  }, [draggableElement]);
 
-  const [preActivationPhase, setPreActivationPhase] = useState(false);
-  const initialMouseCoord = useRef<Coord>({ x: 0, y: 0 });
+  const { isActiveDraggable, setInitialMouseCoord, startDragging } =
+    useBaseDraggable(
+      { ...options, resizeElementBoundingClientRect, boundingClientRect },
+      cleanupFn
+    );
 
   useEffect(() => {
     if (!draggableElement || preActivationPhase) {
@@ -78,10 +55,10 @@ export function useDraggable<DraggablePayload extends DataPayload>(
       if (!event.isPrimary) {
         return;
       }
-      initialMouseCoord.current = {
+      setInitialMouseCoord({
         x: event.clientX,
         y: event.clientY,
-      };
+      });
       setPreActivationPhase(true);
     }
     draggableElement.addEventListener('pointerdown', enterPreActivationPhase);
@@ -91,7 +68,7 @@ export function useDraggable<DraggablePayload extends DataPayload>(
         enterPreActivationPhase
       );
     };
-  }, [draggableElement, preActivationPhase]);
+  }, [draggableElement, preActivationPhase, setInitialMouseCoord]);
 
   // This hook checks the constraints of the pre-activation phase
   useEffect(() => {
@@ -110,99 +87,16 @@ export function useDraggable<DraggablePayload extends DataPayload>(
       if (!event.isPrimary || !draggableElement) {
         return;
       }
-
-      const { x: initialX, y: initialY } = initialMouseCoord.current;
-      const { clientX, clientY } = event;
-
-      const distance = distanceBetween(
-        { x: initialX, y: initialY },
-        { x: clientX, y: clientY }
-      );
-
-      if (distance < distanceConstraint) {
-        return;
-      }
-
-      const elementRect =
-        (action === 'resize-end' || action === 'resize-start') &&
-        resizeElementRef?.current
-          ? resizeElementRef.current.getBoundingClientRect()
-          : draggableElement.getBoundingClientRect();
-
-      const draggableRect = clientDOMRectToRect(elementRect);
-
-      if (action === 'create') {
-        draggableRect.x =
-          axis === 'x' || axis === 'xy' ? clientX : draggableRect.x;
-        draggableRect.y =
-          axis === 'y' || axis === 'xy' ? clientY : draggableRect.y;
-      }
-
-      const mouseCoord = { x: clientX, y: clientY };
-
-      const draggable = {
-        _identity,
-        payload: { type, data },
-        action,
-        animated,
-        cursor,
-        axis,
-        cleanupFn: () => setPreActivationPhase(false),
-      } satisfies DraggableItem;
-
-      store.setState(state => {
-        // Drag is starting, take a snapshot of Rect of all Droppables
-        const droppableRects = state.droppables.map(item =>
-          item.boundingClientRect()
-        );
-        const droppableUpdater = getDroppableCollisionUpdate(
-          type,
-          state.droppables,
-          draggableRect,
-          mouseCoord,
-          collisionDetectionRef.current
-        );
-        return {
-          activeDraggable: draggable,
-          lastKnownActiveDraggabble: draggable,
-          initialDraggableRect: draggableRect,
-          currentDraggableRect: draggableRect,
-          currentTransformedGhostRect: draggableRect,
-          initialMouseCoord: mouseCoord,
-          currentMouseCoord: mouseCoord,
-          droppableRects,
-          ...droppableUpdater,
-        };
-      });
-
-      // FIXME: This is not onDragStart, it will be called
-      // whenever the pointermove is happening
-      onDragStartRef.current?.(draggable, draggableRect);
+      startDragging({ x: event.clientX, y: event.clientY });
     }
+
     window.addEventListener('pointermove', handleStart);
     window.addEventListener('pointerup', exitPreActivationPhase);
     return () => {
       window.removeEventListener('pointermove', handleStart);
       window.removeEventListener('pointerup', exitPreActivationPhase);
     };
-  }, [
-    _identity,
-    action,
-    animated,
-    axis,
-    collisionDetectionRef,
-    cursor,
-    data,
-    distanceConstraint,
-    draggableElement,
-    isActiveDraggable,
-    onDragStartRef,
-    positionOverlay,
-    preActivationPhase,
-    resizeElementRef,
-    store,
-    type,
-  ]);
+  }, [draggableElement, isActiveDraggable, preActivationPhase, startDragging]);
 
   return {
     ref: setDraggableRef,
